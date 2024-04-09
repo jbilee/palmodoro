79efb25c98ae9f449e00b60d1d6a83ab35168f01
jbilee <julishahn@gmail.com>
2024-04-09 10:10:43 +0900

    refactor: Refactor to-do list component - Changed how dispatch function is invoked, by passing in the imported reducer function instead - Adjusted the placement of new to-do input element and added more global styles

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,14 @@
 import { FormControlLabel } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 import { useAppDispatch, useAppSelector } from "../hooks";
+import { toggleTodo } from "../reducers/todosReducer";
 
 const TodoList = () => {
   const todos = useAppSelector((state) => state.todos);
   const dispatch = useAppDispatch();
 
   const handleChange = (id: number, checked: boolean) => {
-    dispatch({ type: "todos/toggleTodo", payload: { id, checked } });
+    dispatch(toggleTodo({ id, checked }));
   };
 
   return (
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,18 @@
+import styled from "styled-components";
 import NewTodo from "./NewTodo";
 import TodoList from "./TodoList";
 
 const Todos = () => {
   return (
-    <>
+    <Wrapper>
       <TodoList />
       <NewTodo />
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  align-self: flex-end;
+`;
 
 export default Todos;
